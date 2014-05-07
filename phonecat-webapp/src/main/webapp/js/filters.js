24be732c1efd55c131442d1ddc090d7517341220
Roger Watkins <roger.watkins@williamhill.com>
2014-05-07 12:36:43 +0100

    Tutorial 9 - Filters

--- phonecat-webapp/src/main/webapp/js/filters.js
+++ phonecat-webapp/src/main/webapp/js/filters.js
@@ -1,3 +1,13 @@
 'use strict';
 
+var TICK = '\u2713';
+var CROSS = '\u2718';
+
 /* Filters */
+
+angular.module('phonecatFilters',
+    []).filter('checkmark', function () {
+        return function (input) {
+            return input ? TICK : CROSS;
+        };
+    });
